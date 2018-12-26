1e33e40c4c74985a2ba3591be1fae58b0388b1e5
DorinDavidov <Dorinyme@gmail.com>
2018-12-26 22:26:57 +0200

    done

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#############################
+# Made by: Dorin
+# Date: 26.12.18
+# Purpose: Lerning
+# Version: 0.0.1 
+#############################
+
+date +%T
+sleep 60
+date +%T
